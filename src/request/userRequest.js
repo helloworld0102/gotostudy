62f0a02818d9ea44aa47dbc794bad5278064a962
helloworld0102 <1732986964@qq.com>
2021-09-06 17:00:24 +0800

    修改跨域的问题和npm install放进package.json的问题

--- src/request/userRequest.js
+++ src/request/userRequest.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const loginRequest =({userName,password})=>{
     return axios({
-        url:"localhost:8080/User/login",
+        url:"http://169.254.183.91:8080/User/login",
         data:{
             userName,
             password
